87fe1168cf021c668f3c835aad680a944bf2e155
Ustun Ertas <ustunertas0@gmail.com>
2022-04-20 20:47:50 +0200

    firstproject

--- src/test/java/IlkClass.java
+++ src/test/java/IlkClass.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class IlkClass {
     public static void main(String[] args) {
 
+        //ilk otomasyonumuzu yapabiliriz artik
         System.setProperty("webdriver.chrome.driver","resources/drivers/chromedriver.exe");
+
+        //mac cullancilarin uzantisinda .exe yok, windovs kullananlarda var
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.amazon.com");
